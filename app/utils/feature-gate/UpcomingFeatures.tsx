0fcd76bcc93ff954f5f189d589d23c412e4f0102
jerry683 <jerry@surf.one>
2025-12-27 18:17:28 +0800

    Update UpcomingFeatures.tsx
    update Upcoming Mainnet Beta Features

--- app/utils/feature-gate/UpcomingFeatures.tsx
+++ app/utils/feature-gate/UpcomingFeatures.tsx
@@ -52,7 +52,7 @@ export function UpcomingFeatures() {
     return (
         <FeaturesTable
             header={
-                <div className="d-flex justify-content-between align-items-center">
+               /* <div className="d-flex justify-content-between align-items-center">
                     <div>
                         <span className="me-2">🚀</span>
                         Upcoming {clusterName(cluster)} Features
@@ -61,7 +61,7 @@ export function UpcomingFeatures() {
                     <Link href={featureGatesPath} className="epoch-link mb-1">
                         View all feature gates
                     </Link>
-                </div>
+                </div>*/
             }
             features={filteredFeatures.filter(feature => !feature.mainnet_activation_epoch)}
             cluster={cluster}
